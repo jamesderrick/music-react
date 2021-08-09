877ad810fe628f41bad5cef5999d32202929d87d
James Derrick <jediblob@aol.com>
2021-08-09 11:10:39 +0100

    add artist star ratings

--- src/components/ArtistList/ArtistList.js
+++ src/components/ArtistList/ArtistList.js
@@ -22,21 +22,38 @@ export default function ArtistList(props) {
         return () => { isLoading = false };
     }, [info])
 
+    function updateRating(id,rating) {
+        
+        try {
+            const { data } = axios.post(`https://react-music-demo.herokuapp.com/artist/${id}`, {
+                rating: rating
+            })
+        } catch (err) {
+            console.log(err)
+        }
+
+        console.log('data updated')
+        setInfo(info)
+
+    }
+
     const artists = [];
 
     const renderArtistCards = (data) => {
-        console.log(data);
         data.forEach(artist => {
             artists.push(<div key={artist.id}>
                 <Card 
+                    id={artist.id}
                     heading={artist.artist}
                     image={artist.artist_img}
                     subHeading={artist.genre}
                     content={artist.intro.length > 200 ? artist.intro.substring(0,200) + '...' : artist.intro}
+                    rating={artist.rating}
                     button= {{
                         label: "View Albums",
                         link: `/artist/${artist.id}`
                     }}
+                    updateRating={updateRating}
                 />
             </div>)
         })
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import StarRatings from 'react-star-ratings'
 import './Card.css'
 
 export default function Card(props) {
+
+    const [rating, setRating] = useState(props.rating);
+
+    function changeRating(newRating,name) {
+        setRating(newRating)
+        updateRating(name,newRating)
+    }
+
+    function updateRating(id,rating) {
+        props.updateRating(id,rating)
+    }
+
     return (
         <div className="card">
             <Link to={props.button.link}>
                 <h1>
                     {props.heading}
-                    {console.log(props.image)}
                     <img src={props.image} />
                 </h1>
                 <hr></hr>
@@ -16,6 +28,14 @@ export default function Card(props) {
                 <p>{props.content}</p>
                 <button>{props.button.label}</button>
             </Link>
+            <StarRatings 
+                rating={rating} 
+                changeRating={changeRating}
+                starDimension="20px"
+                starSpacing="1px"
+                starRatedColor="gold"
+                name={`${props.id}`}
+            />
         </div>
     )
 }
